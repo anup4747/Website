416be3fa227a189b2bff72d0881316c01ff9bc7d
anup <anupdtarwade@gamil.com>
2023-08-19 13:53:43 +0530

    Operators
    Learning Operators in JS

---
+++ JS/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+console.log("Assignment Operatores")
+
+let assignment =1;
+assignment+=5 // same as assignment = assignment + 5
+console.log ("a is now = " ,a)
+assignment-=5 // same as assignment = assignment + 5
+console.log ("a is now = " ,a)
+assignment*=5 // same as assignment = assignment + 5
+console.log ("a is now = " ,a)
+assignment/=5 // same as assignment = assignment + 5
+console.log ("a is now = " ,a)
--- JS/tut5.js
+++ JS/tut5.js
@@ -1,21 +1,76 @@
-console.log("Operarots in JS")
+// console.log("Operarots in JS")
 
-let a = 45
-let b = 4
+// let a = 45
+// let b = 4
 
-console.log("a =", a)
-console.log("b =", b)
+// console.log("a =", a)
+// console.log("b =", b)
 
-console.log("a+b = ",a+b)
-console.log("a-b = ",a-b)
-console.log("a*b = ",a*b)
-console.log("a/b = ",a/b)
-console.log("a**b = ",a**b)
-console.log("a%b = ",a%b)
+// console.log("a+b = ",a+b)
+// console.log("a-b = ",a-b)
+// console.log("a*b = ",a*b)
+// console.log("a/b = ",a/b)
+// console.log("a**b = ",a**b)
+// console.log("a%b = ",a%b)
 // console.log("a++ = ",a++)
 // console.log("++a = ",++a)
 // console.log("a-- = ",a--)
 // console.log("--a = ",--a)
 // console.log("a = ",a)
 
+// ********************************************************************************************
 
+// console.log("Assignment Operatores")
+
+// let assignment =1;
+// assignment+=5 // same as assignment = assignment + 5
+// console.log ("a is now = " ,assignment)
+// assignment-=5 // same as assignment = assignment + 5
+// console.log ("a is now = " ,assignment)
+// assignment*=5 // same as assignment = assignment + 5
+// console.log ("a is now = " ,assignment)
+// assignment/=5 // same as assignment = assignment + 5
+// console.log ("a is now = " ,assignment).
+
+// console.log("comparision Operators")
+
+// let comp1 = 6
+// let comp2 = 7
+// // let comp2 = '6'
+
+// console.log("comp1 = " ,comp1)
+// console.log("comp2 = " ,comp2)
+
+// console.log("comp1 == comp2 is ", comp1 == comp2)
+// console.log("comp1 != comp2 is ", comp1 != comp2)
+// console.log("comp1 === comp2 is ", comp1 === comp2)
+// console.log("comp1 !== comp2 is ", comp1 !== comp2)
+// console.log("comp1 > comp2 is ", comp1 > comp2)
+// console.log("comp1 < comp2 is ", comp1 < comp2)
+// console.log("comp1 >= comp2 is ", comp1 >= comp2)
+// console.log("comp1 <= comp2 is ", comp1 <= comp2)
+
+// **************************************************************************************************************************
+
+// console.log("Logical Operator")
+
+// let x= 5
+// let y=6
+
+// console.log("AND operator")
+
+// console.log(x<y && x==5) 
+// console.log(x>y && x==5)
+
+// console.log("OR operator")
+
+// console.log(x<y || x==5) 
+// console.log(x>y || x==5)
+// console.log(x>y || x!=5)
+
+// console.log("NOT operator")
+
+// console.log(!false)
+// console.log(!true)
+
+ 
