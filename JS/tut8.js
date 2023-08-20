8d84c770787036dab0a121992551bd8cfef62859
anup <anupdtarwade@gamil.com>
2023-08-20 10:08:55 +0530

    tut8
    Learning loops java script

---
+++ JS/tut8.js
@@ -0,0 +1,58 @@
+console.log("loops")
+
+// program to print the n number of natural numbers
+
+// const prompt = require("prompt-sync")({sigint:true})
+
+// let n = prompt("enter the number : ")
+
+// n = Number.parseInt(n)
+
+// for(let i=0 ; i<=n ; i++){
+//     console.log(i)
+// }
+
+// **********************************************************************************************************
+
+// program to add n number of natural numbers
+
+// const prompt = require("prompt-sync")({sigint:true})
+
+// let n = prompt("enter the number : ")
+
+// n = Number.parseInt(n)
+
+// let sum = 0
+
+
+// console.log(typeof sum)
+
+// for(let i=0 ; i<n ; i++){
+//     sum += (i+1)
+// }
+
+// console.log("The sum of natural number is : "+ sum)
+
+// ******************************************************************************************************
+
+// // Now for-in loop AND for-of loop
+
+// let obj = {
+//     Harry:45,
+//     Abhishek:45,
+//     Aman:68,
+//     Krutika:45,
+//     Shubham:65
+// }
+
+// // for-in loop 
+
+// for ( let a in obj){
+//     console.log("Marks of " + a + " are " + obj[a])
+// }
+
+// // for-of loop
+
+// for ( let b of "Abhishek"){
+//     console.log(b)
+// }
