4561a944de71dac256b1937f141be12e2cee88c9
anup <anupdtarwade@gamil.com>
2023-08-20 10:21:23 +0530

    tut9
    learning while and do-while loop in js

---
+++ JS/tut9.js
@@ -0,0 +1,29 @@
+console.log("While loops AND do-while loop.......")
+
+// while loop
+
+// const prompt = require("prompt-sync")({sigint:true})
+
+// let n = prompt("Enter the value of n :")
+// n = Number.parseInt(n)
+// let i=0
+
+// while(i<n){
+//     console.log(i)
+//     i++
+// }
+
+// do-while loop
+
+// const prompt = require("prompt-sync")({sigint:true})
+
+// let n = prompt("Enter the value of n :")
+// n = Number.parseInt(n)
+// let i=0
+
+// do{
+//     console.log(i)
+//     i++
+// }while(i<n)
+
+// In this for loop the first action is done without any condition 
